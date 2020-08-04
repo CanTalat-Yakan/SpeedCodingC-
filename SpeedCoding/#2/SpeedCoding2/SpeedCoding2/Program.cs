815e765f2628a196a0287c1ff262c083f5ee14bb
CanTalat-Yakan <can.talat.yakan@gmail.com>
2020-08-04 15:57:28 +0200

    init

---
+++ SpeedCoding/#2/SpeedCoding2/SpeedCoding2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeedCoding2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
