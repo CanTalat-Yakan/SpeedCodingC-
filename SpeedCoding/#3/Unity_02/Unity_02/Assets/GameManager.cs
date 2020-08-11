e343b2ad418b68f2b5932cde549a2b691ad942b7
CanTalat-Yakan <can.talat.yakan@gmail.com>
2020-08-11 19:43:02 +0200

    nicht geschafft D:

---
+++ SpeedCoding/#3/Unity_02/Unity_02/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[DefaultExecutionOrder(50)]
+public class GameManager : MonoBehaviour
+{
+    List<Enemy> m_enemyList = new List<Enemy>();
+
+    void Awake()
+    {
+        Enemy[] m_enemyArray = FindObjectsOfType<Enemy>();// GetComponents<Enemy>();
+        m_enemyList = m_enemyArray.ToList();
+        //AccessController.Instance.GetComponent<AccessController>().enabled = false;
+    }
+
+    void Update()
+    {
+        //CheckHp();
+    }
+
+    void CheckHp()
+    {
+        
+        Application.LogCallback();
+        foreach (Enemy enemy in m_enemyList)
+        {
+            Debug.Log(enemy.CurrentHP);
+            if (enemy.CurrentHP == 0)
+            {
+                //enemy.CancelInvoke();
+                Destroy(enemy.gameObject);
+            }
+        }
+    }
+
+}
